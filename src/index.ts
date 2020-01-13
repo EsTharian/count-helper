feb547fa75d7a3760d6f7e22993725b5b97b662e
Muhammed Taha Ayan <muhammed.ayan@akinon.com>
2020-01-13 11:42:09 +0300

    Development [1]

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 interface ITarget {
-  datetime: string | bigint | number
-  targetDOM: HTMLElement
+  datetime: Date
+  targetDOMs: object
+  now: Date
+  options: Options
 
   fullYear(dom: HTMLElement): number
   fullDay(dom: HTMLElement): number
@@ -14,56 +16,87 @@ interface ITarget {
   second(dom: HTMLElement): string
 }
 
+interface Options {
+  yearDOM: HTMLElement
+  dayDOM: HTMLElement
+  hourDOM: HTMLElement
+  minuteDOM: HTMLElement
+  secondDOM: HTMLElement
+}
+
 class CountDown implements ITarget {
-  public datetime: string | bigint | number
-  public targetDOM: HTMLElement
+  public datetime: Date
+  public targetDOMs: object
+  public now: Date
+  public options: Options
 
-  constructor(datetime: string | bigint | number, targetDOM: HTMLElement) {
-    this.datetime = datetime
-    this.targetDOM = targetDOM
+  constructor(datetime: string | number | Date, options: Options) {
+    this.options = options
+
+    this.datetime = typeof datetime === 'number' || typeof datetime === 'string' ? new Date(datetime) : datetime
+    this.now = new Date()
   }
 
-  public fullYear(dom: HTMLElement = null): number {
-    if(dom) {
-      dom.innerText = ''
-    }
-    return 1
+  public monthDays(date: Date) {
+    const d = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    return d.getDate();
   }
 
-  public fullDay(dom: HTMLElement = null): number {
-    return 1
+  public fullYear(): number {
+    return this.datetime.getFullYear() - this.now.getFullYear()
   }
 
-  public fullHour(dom: HTMLElement = null): number {
-    return 1
+  public fullDay(): number {
+    return Math.floor((this.fullHour() / 24))
   }
 
-  public fullMinute(dom: HTMLElement = null): number {
-    return 1
+  public fullHour(): number {
+    return Math.floor((this.fullMinute() / 60))
   }
 
-  public fullSecond(dom: HTMLElement = null): number {
-    return 1
+  public fullMinute(): number {
+    return Math.floor((this.fullSecond() / 60))
   }
 
-  public year(dom: HTMLElement = null): string {
-    return "1"
+  public fullSecond(): number {
+    return Math.floor((this.datetime.getTime() - this.now.getTime()) / 1000)
   }
 
-  public day(dom: HTMLElement = null): string {
-    return "1"
+  public year(): string {
+    const year = this.datetime.getFullYear() - this.now.getFullYear()
+    return year.toString().padStart(2, '0')
   }
 
-  public hour(dom: HTMLElement = null): string {
-    return "1"
+  public day(): string {
+    return Math.floor(this.fullDay() % 365).toString().padStart(2, '0')
   }
 
-  public minute(dom: HTMLElement = null): string {
-    return "1"
+  public hour(): string {
+    return Math.floor(this.fullHour() % 24).toString().padStart(2, '0')
   }
 
-  public second(dom: HTMLElement = null): string {
-    return "1"
+  public minute(): string {
+    return Math.floor(this.fullMinute() % 60).toString().padStart(2, '0')
+  }
+
+  public second(): string {
+    return Math.floor(this.fullSecond() % 60).toString().padStart(2, '0')
+  }
+
+  public initialize(): NodeJS.Timeout {
+    return setInterval(() => {
+      this.now = new Date()
+
+      console.log(this.second())
+
+      if(this.second() === '59') {
+        console.log(this.minute())
+
+        this.options.hourDOM.innerHTML = this.minute() === '59' && this.hour()
+        this.hour() === '23' && console.log(this.day())
+        this.day() === '364' && console.log(this.year())
+      }
+    }, 1000)
   }
 }
 
